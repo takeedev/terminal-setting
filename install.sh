df19092c643192bef0ec9455f83899232ee6f460
Takee Dev <peerapolkoeram@gmail.com>
2026-04-12 20:00:13 +0700

    add install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+echo "Start setup"
+
+# detect OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "mac"
+
+  # check brew
+  if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  brew update
+  brew install zsh git curl unzip fzf
+
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "Linux"
+
+  sudo apt update
+  sudo apt install -y zsh git curl unzip fzf
+
+fi
+
+# install mise
+if [ ! -f "$HOME/.local/bin/mise" ]; then
+  curl https://mise.run | sh
+fi
+
+# add mise to zshrc
+grep -qxF 'eval "$(~/.local/bin/mise activate zsh)"' ~/.zshrc || \
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+
+# install oh-my-posh
+if ! command -v oh-my-posh &> /dev/null; then
+  curl -s https://ohmyposh.dev/install.sh | bash -s
+fi
+
+# add oh-my-posh config
+grep -qxF 'eval "$(oh-my-posh init zsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/M365Princess.omp.json')"' ~/.zshrc || \
+echo 'eval "$(oh-my-posh init zsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/M365Princess.omp.json')"' >> ~/.zshrc
+
+# fzf setup
+[ -f ~/.fzf.zsh ] || ~/.fzf/install --all
+
+echo "Setup complete! and Restart terminal"
